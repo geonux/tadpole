b28866d1f9ffe0c1aae280511bc105df4a2a0bd4
Geonux <gaudissart@gmail.com>
2024-04-27 15:30:37 +0200

    Tapole config refactoring

--- tadpoleConfig.py
+++ tadpoleConfig.py
@@ -1,110 +1,71 @@
-import os
-import string
 import configparser
+import logging
+from os import mkdir
+from os.path import exists
+from pathlib import Path
 
-class TadpoleConfig():
-    _static_TadpoleFolder = os.path.join(os.path.expanduser('~'), '.tadpole')
-    _static_TadpoleConfigFile = os.path.join(_static_TadpoleFolder, 'tadpole.ini')
-    # [tadpole]
-    _static_general = "tadpole"
-    _static_general_userDirectory = "user_directory"
-    _static_general_userDirectory_DEFAULT = ""
-    # [thumbnails]
-    _static_thumbnails = "Thumbnails"
-    _static_thumbnails_view = "ViewInTable"
-    _static_thumbnails_view_DEFAULT = "False"
-    _static_thumbnails_overwrite = "overwrite"
-    _static_thumbnails_overwrite_DEFAULT = "False"
-    _static_thumbnails_download = "download"
-    _static_thumbnails_download_DEFAULT = "0"
 
-    
+class TadpoleConfig:
+    _tadpole_folder = Path.home() / ".tadpole"
+    _tadpole_config_file = _tadpole_folder / "tadpole.ini"
+
+    _DEFAULT_CONFIG = """
+        [tadpole]
+        user_directory = 
+
+        [Thumbnails]
+        viewintable = False
+        overwrite = False
+        download = 0
+    """
+    _MAIN_SECTION = "tadpole"
+    _THUMB_SECTION = "Thumbnails"
+
     def __init__(self):
         super().__init__()
-        print(f"establishing tadpole config")
+        logging.info(f"establishing tadpole config")
         self.config = configparser.ConfigParser()
-        #if not os.path.exists(self._static_TadpoleFolder):
-        #    os.makedirs(self._static_TadpoleFolder, exist_ok=True)
-        # Check if config file exists on the device
-        if not os.path.exists(self._static_TadpoleConfigFile):
-            #Config file not found, create a new one            
-            with open(self._static_TadpoleConfigFile, 'w') as newConfigFile:
-                self.config.write(newConfigFile)
-          
-        # Double check that the config file now exists
-        if not os.path.exists(self._static_TadpoleConfigFile):
-            #TODO replace this with a proper exception
-            raise Exception
-          
-        #open the config  
-        self.config.read(self._static_TadpoleConfigFile)
-          
-        # make sure all the right keys exist, if not create the defaults
-        # [Tadpole]
-        if not self.config.has_section(self._static_general):
-            self.config[self._static_general] = {}
-        if not self.config.has_option(self._static_general, self._static_general_userDirectory):
-            self.config[self._static_general][self._static_general_userDirectory] = self._static_general_userDirectory_DEFAULT
-        # [Thumbnails]
-        if not self.config.has_section(self._static_thumbnails):
-            self.config[self._static_thumbnails] = {}
-        if not self.config.has_option(self._static_thumbnails, self._static_thumbnails_view):
-            self.config[self._static_thumbnails][self._static_thumbnails_view] = self._static_thumbnails_view_DEFAULT
-        if not self.config.has_option(self._static_thumbnails, self._static_thumbnails_overwrite):
-            self.config[self._static_thumbnails][self._static_thumbnails_overwrite] = self._static_thumbnails_overwrite_DEFAULT
-        
-        
-        # Update the config out to file
-        with open(self._static_TadpoleConfigFile, 'w') as newConfigFile:
-            self.config.write(newConfigFile)
-    
+
+        # Load default value
+        self.config.read_string(self._DEFAULT_CONFIG)
+
+        # Overwrite default value with user's values
+        try:
+            self.config.read(self._tadpole_config_file)
+        except:
+            # Problem reading file so creating a new one
+            if not exists(self._tadpole_folder):
+                mkdir(self._tadpole_folder)
+
+        # Wrtite config file with merged default values
+        with open(self._tadpole_config_file, "w") as fp:
+            self.config.write(fp)
+
     def setVariable(self, section, option, value):
-        #Check the section exists, if it doesnt then create it
-        if not self.config.has_section(section):
-            self.config[section] = {}
         self.config[section][option] = value
-        with open(self._static_TadpoleConfigFile, 'w') as newConfigFile:
-            self.config.write(newConfigFile)
-            
-    def getVariable(self, section, option, default):
-        if (self.config.has_option(section,option)):
-            return self.config[section][option]
-        print(f"Returning default for ({section})({option})")
-        return default
-    
+        with open(self._tadpole_config_file, "w") as fp:
+            self.config.write(fp)
+
     def setLocalUserDirectory(self, location):
-        print(f"Setting LocalUserDirectory to ({location})")
-        self.setVariable(self._static_general, self._static_general_userDirectory, location)
-                
-    """
-    Checks if the user_directory value has been set in the tadpole section
-    If it has then the value is returned
-    If it hasnt then the default value is returned
-    """
+        self.setVariable(self._MAIN_SECTION, "user_directory", location)
+
     def getLocalUserDirectory(self):
-        return self.getVariable(self._static_general,self._static_general_userDirectory,self._static_general_userDirectory_DEFAULT)
+        return self.config[self._MAIN_SECTION].get("user_directory")
 
     def setViewThumbnailsInTable(self, enabled: bool):
-        print(f"Setting ViewThumbnailsInTable to ({enabled})")
-        self.setVariable(self._static_thumbnails, self._static_thumbnails_view, ("True" if enabled else "False"))
-    
+        self.setVariable(self._THUMB_SECTION, "viewintable", enabled)
+
     def getViewThumbnailsInTable(self):
-        view = self.getVariable(self._static_thumbnails,self._static_thumbnails_view,self._static_general_userDirectory_DEFAULT)
-        return view == "True"
+        return self.config[self._THUMB_SECTION].getboolean("viewintable")
 
     def setThumbnailDownload(self, enabled: bool):
-        print(f"Setting ThumbnailDownload to ({enabled})")
-        self.setVariable(self._static_thumbnails, self._static_thumbnails_download, ("1" if enabled else "0"))
-    
-    def getThumbnailDownload(self):
-        view = self.getVariable(self._static_thumbnails,self._static_thumbnails_download,self._static_thumbnails_download_DEFAULT)
-        return view == "1"
+        self.setVariable(self._THUMB_SECTION, "download", enabled)
 
-       
+    def getThumbnailDownload(self):
+        return self.config[self._THUMB_SECTION].getboolean("download")
+
     def setThumbnailOverwrite(self, enabled: bool):
-        print(f"Setting ThumbnailOverwrite to ({enabled})")
-        self.setVariable(self._static_thumbnails, self._static_thumbnails_overwrite, ("True" if enabled else "False"))
-    
+        self.setVariable(self._THUMB_SECTION, "overwrite", enabled)
+
     def getThumbnailOverwrite(self):
-        view = self.getVariable(self._static_thumbnails,self._static_thumbnails_overwrite,self._static_thumbnails_overwrite_DEFAULT)
-        return view == "True"
+        return self.config[self._THUMB_SECTION].getboolean("overwrite")
